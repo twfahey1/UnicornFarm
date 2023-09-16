e3882b95cf4a45c6771e7969f9c56c60b2e3dc57
Tyler Fahey <twfahey1@gmail.com>
2023-09-15 22:37:02 -0500

    Revise logic to use simple left and right animation logic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -27,7 +28,19 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_h = velocity.x > 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.flip_v = velocity.y < 0
+
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
